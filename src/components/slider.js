1915e861b1fdb57b1fdd871dd5f38d02be07e056
Alireza Farahani <alireza.mastery@gmail.com>
2020-11-14 14:14:56 +0330

    second code

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,8 +1,40 @@
 import React from "react"
 
-class Slider extends React.Component{
+class Slider extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            insideText: this.props.text[0],
+            toggle: true
+        }
+        console.log(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
 
+    handleClick(event) {
+        if (this.state.toggle) {
+            console.log('in if')
+            this.setState({
+                insideText: this.props.text[1],
+                toggle: false
+            })
+        } else {
+            console.log('in else')
+            this.setState({
+                insideText: this.props.text[0],
+                toggle: true
+            })
+        }
+    }
 
+    render() {
+        return (
+            <div>
+                <h1>{this.state.insideText}</h1>
+                <button onClick={this.handleClick}>click</button>
+            </div>
+        )
+    }
 }
 
 export default Slider
