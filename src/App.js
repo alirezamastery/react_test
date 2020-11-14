72842a67cf5943c111a81458b792e52134808c97
Alireza Farahani <alireza.mastery@gmail.com>
2020-11-14 14:16:24 +0330

    second code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Slider
-  from "./components/slider";
+    from "./components/slider";
+
 function App() {
-  return (
-    <div className="App">
-      <Slider />
-    </div>
-  );
+    const text1 = "this is a test"
+    const text2 = "not any more"
+
+    return (
+        <div className="App">
+            <Slider text={[text1, text2]}/>
+        </div>
+    );
 }
 
 export default App;
